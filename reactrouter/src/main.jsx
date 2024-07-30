f626dbef84c9c2e458abfea71e376df96aa8f0ce
Sushan Karki <147791962+Sushan4455@users.noreply.github.com>
2024-07-30 11:23:57 +0000

    commit

--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -2,39 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import Courses from './components/Contact/Courses.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children : [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "/About",
-        element: <About />
-      },
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children : [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "/About",
+//         element: <About />
+//       },
 
-      {
-        path: "/Contact",
-        element: <Contact />
-      },
+//       {
+//         path: "/Contact",
+//         element: <Contact />
+//       },
 
-      {
-        path: "/Courses",
-        element: <Courses />
-      }
-    ] 
-  }
-])
+//       {
+//         path: "/Courses",
+//         element: <Courses />
+//       }
+//     ] 
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+   <Route path='/' element={<Layout />}>
+    <Route path='' element={<Home />}/>
+    <Route path='About' element={<About />}/>
+    <Route path='Contact' element={<Contact />}/>
+    <Route path='Courses' element={<Courses />}/>
+   </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider  router={router} />
